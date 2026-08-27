bdc13a8e692726810c3d768b1fd6e57149593c7d
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2026-08-27 13:13:01 +0530

    config - initializer

--- apps/initializer/app/config/config.py
+++ apps/initializer/app/config/config.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-from pathlib import Path
 
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -7,11 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     APP_NAME             : str = "Insertion.AI"
-    APP_VERSION          : str = "1.0.0"
-
-    USER_NAME            : str
-
-    DATABASE_URL         : str
+    APP_VERSION          : str = "2.0.0"
 
     OPENROUTER_URL       : str
 
@@ -21,27 +16,6 @@ class Settings(BaseSettings):
     OPENROUTER_API_KEY_4 : SecretStr
 
     OPENAI_API_KEY       : SecretStr
-
-    GITHUB_TOKEN         : str
-    
-    GITHUB_OWNER         : str
-    GITHUB_REPO          : str
-    GITHUB_BRANCH        : str = "main"
-
-    HIREBASE_API_KEY_1   : str = ""
-    HIREBASE_API_KEY_2   : str = ""
-    HIREBASE_API_KEY_3   : str = ""
-
-    ZONE_INFO            : str
-
-    SYNC_DATA_PATH       : Path
-    
-    SCHEDULE_PATH        : Path
-    RESUME_PATH          : Path
-    JOB_SEARCH_PATH      : Path
-
-    DAILY_QUESTION_GENERATION_COUNT : int
-    DAILY_JON_SEARCH_LIMIT          : int
 
     model_config = SettingsConfigDict(
         env_file = ".env",
