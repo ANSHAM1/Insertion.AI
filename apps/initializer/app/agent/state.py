8bc48c67642caead71fabac5594a8a114141e2b0
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2026-08-29 14:34:43 +0530

    master workflow - temporal initialization and folders

--- apps/initializer/app/agent/state.py
+++ apps/initializer/app/agent/state.py
@@ -2,17 +2,14 @@ from typing import Any, TypedDict
 
 from langchain_core.prompt_values import PromptValue
 
-from src.database.repository import RssRepository, DailyScheduleRepository
 # from src.validators.planner_output import PlannerOutput
 
 
 
 
 class PlannerState(TypedDict):
+    
     already_synced : bool
-
-    rss_repo       : RssRepository
-    schedule_repo  : DailyScheduleRepository
 
     prev_schedules : dict[str, dict[str, Any]]
     curr_schedule  : PlannerOutput | None
